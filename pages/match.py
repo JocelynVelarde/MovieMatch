541c85077e48b5e8a522fa2a66b18fc73cca189d
JocelynVelarde <a01285780@itesm.mx>
2024-02-13 07:53:00 -0600

    updated sample pages

---
+++ pages/match.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title('Match')
+st.write('Welcome to the match page')
---
+++ pages/profile.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title('Profile')
+st.write('Welcome to the profile page')
